4c8c2f5023c64b65b157d9fc22153901df265c7c
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-10 11:44:07 +0200

    Creating WebClient

---
+++ src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
@@ -0,0 +1,7 @@
+package guru.springframework.reactivebeerclient.config;
+
+public class WebClientProperties {
+    public static final String BASE_URL = "https://api.springframework.guru"; // "http://localhost:8080";
+
+
+}
