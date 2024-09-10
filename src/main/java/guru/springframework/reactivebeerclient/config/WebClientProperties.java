2ae560be4bbe328611b9e2cb71c95f45a41213e3
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-10 11:55:35 +0200

    WebClient Get Operations

--- src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
+++ src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
@@ -1,7 +1,8 @@
 package guru.springframework.reactivebeerclient.config;
 
 public class WebClientProperties {
-    public static final String BASE_URL = "https://api.springframework.guru"; // "http://localhost:8080";
 
+    public static final String BASE_URL = "https://api.springframework.guru"; // "http://localhost:8080";
+    public static final String BEER_V1_PATH = "/api/v1/beer";
 
 }
--- src/test/java/guru/springframework/reactivebeerclient/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/reactivebeerclient/client/BeerClientImplTest.java
@@ -1,12 +1,17 @@
 package guru.springframework.reactivebeerclient.client;
 
 import guru.springframework.reactivebeerclient.config.WebClientConfig;
+import guru.springframework.reactivebeerclient.model.BeerPagedList;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class BeerClientImplTest {
 
     BeerClientImpl beerClient;
@@ -22,6 +27,14 @@ class BeerClientImplTest {
 
     @Test
     void listBeers() {
+        Mono<BeerPagedList> beerPagedListMono =
+                beerClient.listBeers(null, null, null, null, null);
+
+        BeerPagedList pagedList = beerPagedListMono.block();
+
+        assertThat(pagedList).isNotNull();
+        assertThat(pagedList.getContent().size()).isGreaterThan(0);
+        log.info("Beer List: {}", pagedList);
     }
 
     @Test
